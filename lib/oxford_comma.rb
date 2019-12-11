b74cfa157973e9dbd55bf7a34508c2a68fb303b7
Scott Hoisington <hoyzington@gmail.com>
2019-12-11 19:52:52 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
-def oxford_comma(array)
+require "pry"
 
+three = ["1", "2", "3"]
+
+def oxford_comma(array)
+  if array.size < 3
+    return array.join(" and ")
+  else
+    string_end = ", and #{array.pop}"
+binding.pry    
+    string = array.join(", ")
+binding.pry
+    return string + string_end
+  end
 end
+
+oxford_comma(three)
