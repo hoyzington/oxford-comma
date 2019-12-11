9103f9344e8c1aada4dc8d3475881ef2d2cf35e0
Scott Hoisington <hoyzington@gmail.com>
2019-12-11 20:00:57 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
 def oxford_comma(array)
-
+  if array.size < 3
+    return array.join(" and ")
+  else
+    string_end = ", and #{array.pop}"
+    string = array.join(", ")
+    return string + string_end
+  end
 end
